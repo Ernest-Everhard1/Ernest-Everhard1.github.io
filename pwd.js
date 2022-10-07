5b46a5ff3e247ae8f498cc80c142d4a788726acb
Ernest-Everhard1 <115318190+Ernest-Everhard1@users.noreply.github.com>
2022-10-07 16:24:22 -0400

    Add files via upload

---
+++ pwd.js
@@ -0,0 +1,6 @@
+
+				document.getElementById("pwd").addEventListener('keydown', function(event){
+					if(event.keyCode == 13){
+						window.location.href = "file:///C:/Users/user/Documents/VOIDRESEARCH/home.html";
+					}
+				})
